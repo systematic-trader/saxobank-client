d59c035ba81bec36964f0e2b372acaf25cb35eb7
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-09-06 16:04:07 +0200

    Add SaxoOauth class for initiating OAuth flow and token management

--- src/cli.ts
+++
@@ -1,7 +0,0 @@
-import { authorize } from './authentication/authorize.ts'
-
-const authentication = await authorize()
-
-// todo refactor this into an actual cli?
-// deno-lint-ignore no-console
-console.log(authentication)
