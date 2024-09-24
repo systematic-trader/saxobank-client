c0a90809e8e18c712a6f384b4c3407b52575d3c4
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-09-24 10:34:42 +0200

    Use account key in test

--- src/service-groups/reference-data/standard-dates/__tests__/forward-tenor.test.ts
+++ src/service-groups/reference-data/standard-dates/__tests__/forward-tenor.test.ts
@@ -1,15 +1,23 @@
 import { expect } from 'std/expect/mod.ts'
 import { test } from 'std/testing/bdd.ts'
-import { HTTPClient } from '../../../../http-client.ts'
-import { ResourceClient } from '../../../../resource-client.ts'
-import { ForwardTenor } from '../forward-tenor.ts'
+import { SaxoBankClient } from '../../../../../mod.ts'
+import { SaxoBank24HourToken } from '../../../../authentication/saxobank-24-hour-token.ts'
 
-test('reference-data/standard-dates/forwardtenor', async () => {
-  const resource = new ForwardTenor({
-    client: new ResourceClient({ client: HTTPClient.fromEnvironment() }).appendPath('ref/v1/standarddates'),
+test('reference-data/standard-dates/forward-tenor', async () => {
+  const saxoBankClient = new SaxoBankClient({
+    prefixURL: 'https://gateway.saxobank.com/sim/openapi',
+    authorization: new SaxoBank24HourToken(),
   })
 
-  const forwardTenor = await resource.get({ Uic: 22041762, AccountKey: 'Bla' })
+  const [account] = await saxoBankClient.portfolio.accounts.me.get()
+  if (account === undefined) {
+    throw new Error('No account found')
+  }
+
+  const forwardTenor = await saxoBankClient.referenceData.standarddates.forwardTenor.get({
+    AccountKey: account.AccountKey,
+    Uic: 22041762,
+  })
 
   expect(forwardTenor).toBeDefined()
 })
