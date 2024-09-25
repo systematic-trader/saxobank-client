53913630dd65fc4e27a5ecfb077cef65dfb4a28c
Jacob Madsen <jacob@webnuts.com>
2024-09-25 12:47:16 +0200

    Better typing

--- src/types/derives/asset-type.ts
+++ src/types/derives/asset-type.ts
@@ -1,6 +1,6 @@
-import { enums, type GuardType } from 'https://raw.githubusercontent.com/systematic-trader/type-guard/main/mod.ts'
+import { enums } from 'https://raw.githubusercontent.com/systematic-trader/type-guard/main/mod.ts'
 
-export type AssetType = GuardType<typeof AssetType>
+export type AssetType = typeof AssetTypeValues[number]
 
 export const AssetTypeValues = [
   /* Not tradeable */
