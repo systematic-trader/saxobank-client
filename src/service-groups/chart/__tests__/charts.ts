a0edb5f2d19cb4f7d3cb5f046e40c640fdea3f2e
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-01 10:46:45 +0200

    Refactor charts resource to use an interface for each asset type + minor tweaks to response model

--- src/service-groups/chart/__tests__/charts.ts
+++ src/service-groups/chart/__tests__/charts.ts
@@ -2,8 +2,14 @@ import { expect } from 'std/expect/mod.ts'
 import { describe, test } from 'std/testing/bdd.ts'
 import { SaxoBankClient } from '../../../../mod.ts'
 import { SaxoBank24HourToken } from '../../../authentication/saxobank-24-hour-token.ts'
-import { extractEntries } from '../../../utils.ts'
+import { HTTPClientError } from '../../../http-client.ts'
 import type { ChartsParameters } from '../charts.ts'
+
+const MAXIMUM_INSTRUMENTS_PER_ASSET_TYPE = 500
+
+function progress(current: number, total: number) {
+  return `${String(current).padStart(String(total).length, '0')}/${total}`
+}
 
 describe('charts/chart', () => {
   const saxoBankClient = new SaxoBankClient({
@@ -12,123 +18,91 @@ describe('charts/chart', () => {
   })
 
   test('Getting chart data for asset type', async ({ step }) => {
-    const cases: Record<ChartsParameters['AssetType'], Record<string, { readonly Uic: number }>> = {
-      Bond: {
-        'Nykredit Realkredit 5% 01 Oct 2056, DKK': { Uic: 36413429 },
-        'Germany 0% 10 Oct 2025, EUR': { Uic: 36938891 },
-      },
+    const assetTypeCandidates: ChartsParameters['AssetType'][] = [
+      'Bond',
+      'CompanyWarrant',
+      'CfdOnCompanyWarrant',
+      'ContractFutures',
+      'CfdOnFutures',
+      'Etc',
+      'CfdOnEtc',
+      'Etf',
+      'CfdOnEtf',
+      'Etn',
+      'CfdOnEtn',
+      'Fund',
+      'CfdOnFund',
+      'FxSpot',
+      'Rights',
+      'CfdOnRights',
+      'Stock',
+      'CfdOnStock',
+      'StockIndex',
+      'CfdOnIndex',
+    ] as const
 
-      CompanyWarrant: {
-        'STENOCARE A/S - Warrant': { Uic: 36067046 },
-        'Valaris Ltd.': { Uic: 22736639 },
-      },
-      CfdOnCompanyWarrant: {
-        'Diana Shipping Inc - Warrant': { Uic: 39509019 },
-        'COMPAGNIE FINANCIERE RICHEMONT SA-WRT-Reduce Only': { Uic: 20278036 },
-      },
+    for (const assetType of assetTypeCandidates) {
+      const instruments = await saxoBankClient.referenceData.instruments.get({
+        AssetTypes: [assetType] as const,
+        limit: MAXIMUM_INSTRUMENTS_PER_ASSET_TYPE,
+      })
 
-      ContractFutures: {
-        'Gold - Aug 2025': { Uic: 40758986 },
-        'Light Sweet Crude Oil (WTI) - Dec 2027': { Uic: 37393385 },
-      },
-      CfdOnFutures: {
-        'UK Crude January 2025': { Uic: 42445474 },
-        'Platinum January 2025': { Uic: 42466309 },
-      },
+      await step(assetType, async ({ step: substep }) => {
+        let index = 0
+        for (const instrument of instruments) {
+          switch (instrument.AssetType) {
+            case 'Bond':
+            case 'CompanyWarrant':
+            case 'CfdOnCompanyWarrant':
+            case 'ContractFutures':
+            case 'CfdOnFutures':
+            case 'Etc':
+            case 'CfdOnEtc':
+            case 'Etf':
+            case 'CfdOnEtf':
+            case 'Etn':
+            case 'CfdOnEtn':
+            case 'Fund':
+            case 'CfdOnFund':
+            case 'FxSpot':
+            case 'Rights':
+            case 'CfdOnRights':
+            case 'Stock':
+            case 'CfdOnStock':
+            case 'StockIndex':
+            case 'CfdOnIndex': {
+              const label = `${
+                progress(++index, instruments.length)
+              }: ${instrument.Description} (UIC=${instrument.Identifier})`
 
-      Etc: {
-        'Global X Physical Gold ETC': { Uic: 19357 },
-        'WisdomTree WTI Crude Oil ETC': { Uic: 35385 },
-      },
-      CfdOnEtc: {
-        'Global X Physical Gold ETC': { Uic: 19357 },
-        'WisdomTree WTI Crude Oil ETC': { Uic: 35385 },
-      },
+              await substep(label, async () => {
+                try {
+                  const chart = await saxoBankClient.chart.charts.get({
+                    AssetType: assetType,
+                    Uic: instrument.Identifier,
+                    Horizon: 60,
+                    Count: 3,
+                  })
 
-      Etf: {
-        'VanEck Vectors Gold Miners ETF': { Uic: 35663 },
-        'iShares STOXX EU 600 Oil & Gas UCITS ETF (DE)': { Uic: 39342 },
-      },
-      CfdOnEtf: {
-        'VanEck Vectors Gold Miners ETF': { Uic: 35663 },
-        'iShares STOXX EU 600 Oil & Gas UCITS ETF (DE)': { Uic: 39342 },
-      },
+                  expect(chart).toBeDefined()
+                } catch (error) {
+                  if (error instanceof HTTPClientError && error.statusCode === 403) {
+                    // deno-lint-ignore no-console
+                    console.log(`No access to charts for UIC=${instrument.Identifier} (skipping)`)
+                    return
+                  }
 
-      Etn: {
-        'iPath Bloomberg Commodity Index Total Return ETN': { Uic: 4692063 },
-        'Ipath S&P VIX Short Term Future ETN': { Uic: 13727743 },
-      },
-      CfdOnEtn: {
-        'iPath Bloomberg Commodity Index Total Return ETN': { Uic: 4692063 },
-        'Ipath S&P VIX Short Term Future ETN': { Uic: 13727743 },
-      },
+                  throw error
+                }
+              })
 
-      Fund: {
-        'North European Oil Royalty Trust': { Uic: 54064 },
-        'Sprott Physical Gold Trust': { Uic: 46813 },
-        'Alliance Trust Plc': { Uic: 54064 },
-      },
-      CfdOnFund: {
-        'North European Oil Royalty Trust': { Uic: 54064 },
-        'Sprott Physical Gold Trust': { Uic: 46813 },
-        'Alliance Trust Plc': { Uic: 54064 },
-      },
+              break
+            }
 
-      FxSpot: {
-        'Gold/US Dollar': { Uic: 8176 },
-        'US Dollar/Japanese Yen': { Uic: 42 },
-      },
-
-      Rights: {
-        'Corporacion Financiera Alba SA - Rights': { Uic: 36115376 },
-        'Spineguard - Rights': { Uic: 42828275 },
-      },
-      CfdOnRights: {
-        'Corporacion Financiera Alba SA - Rights': { Uic: 36115376 },
-        'Wolford AG - Rights': { Uic: 33612435 },
-        'LG Display Co. Ltd - ADR-  Rights': { Uic: 40142100 },
-      },
-
-      Stock: {
-        'Apple Inc.': { Uic: 211 },
-        'Tesla Inc.': { Uic: 47556 },
-        'NVIDIA Corp.': { Uic: 1249 },
-      },
-      CfdOnStock: {
-        'Apple Inc.': { Uic: 211 },
-        'Tesla Inc.': { Uic: 47556 },
-        'NVIDIA Corp.': { Uic: 1249 },
-      },
-
-      StockIndex: {
-        'NASDAQ Composite Index': { Uic: 1376 },
-        'OBX Oslo Index': { Uic: 7940 },
-        'Germany 40': { Uic: 4910 },
-      },
-      CfdOnIndex: {
-        'OBX Oslo Index': { Uic: 7940 },
-        'Germany 40': { Uic: 4910 },
-        'US 500': { Uic: 4913 },
-      },
-    } as const
-
-    for (const [AssetType, subcases] of extractEntries(cases)) {
-      if (Object.keys(subcases).length === 0) {
-        continue
-      }
-
-      await step(AssetType, async ({ step: substep }) => {
-        for (const [label, { Uic }] of extractEntries(subcases)) {
-          await substep(label, async () => {
-            const chart = await saxoBankClient.chart.charts.get({
-              AssetType,
-              Uic,
-              Horizon: 60,
-              Count: 3,
-            })
-
-            expect(chart).toBeDefined()
-          })
+            default: {
+              throw new Error(`Unsupported asset type`)
+            }
+          }
         }
       })
     }
@@ -142,15 +116,15 @@ describe('charts/chart', () => {
       Count: 1,
     })
 
-    expect(typeof chart.Data[0]?.CloseAsk).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.CloseBid).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.HighAsk).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.HighBid).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.LowAsk).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.LowBid).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.OpenAsk).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.OpenBid).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.Time).toStrictEqual('string')
+    expect(typeof chart.Data?.[0]?.CloseAsk).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.CloseBid).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.HighAsk).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.HighBid).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.LowAsk).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.LowBid).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.OpenAsk).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.OpenBid).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.Time).toStrictEqual('string')
   })
 
   test('Getting ohlc chart data for stock', async () => {
@@ -161,12 +135,12 @@ describe('charts/chart', () => {
       Count: 1,
     })
 
-    expect(typeof chart.Data[0]?.Close).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.High).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.Low).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.Open).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.Volume).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.Interest).toStrictEqual('number')
-    expect(typeof chart.Data[0]?.Time).toStrictEqual('string')
+    expect(typeof chart.Data?.[0]?.Close).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.High).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.Low).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.Open).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.Volume).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.Interest).toStrictEqual('number')
+    expect(typeof chart.Data?.[0]?.Time).toStrictEqual('string')
   })
 })
--- src/types/records/chart-response.ts
+++ src/types/records/chart-response.ts
@@ -2,10 +2,11 @@ import {
   array,
   type GuardType,
   integer,
+  optional,
   props,
 } from 'https://raw.githubusercontent.com/systematic-trader/type-guard/main/mod.ts'
 import { ChartInfo } from '../derives/chart-info.ts'
-import { ChartSample } from '../derives/chart-sample.ts'
+import { ChartSample, ChartSampleBidAskOHLC, ChartSampleOHLC } from '../derives/chart-sample.ts'
 import { DisplayAndFormat } from '../derives/display-and-format.ts'
 
 export interface ChartResponse extends GuardType<typeof ChartResponse> {}
@@ -23,3 +24,123 @@ export const ChartResponse = props({
   /** Object holding information relevant to displaying the instrument and formatting the samples for charting it. Currently holds the symbol of the instrument, how many decimals samples have, a description of the instrument and what currency it is traded in. */
   DisplayAndFormat,
 })
+
+export const ChartResponseBond = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseBond extends GuardType<typeof ChartResponseBond> {}
+
+export const ChartResponseCompanyWarrant = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCompanyWarrant extends GuardType<typeof ChartResponseCompanyWarrant> {}
+
+export const ChartResponseCfdOnCompanyWarrant = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnCompanyWarrant extends GuardType<typeof ChartResponseCfdOnCompanyWarrant> {}
+
+export const ChartResponseContractFutures = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseContractFutures extends GuardType<typeof ChartResponseContractFutures> {}
+
+export const ChartResponseCfdOnFutures = ChartResponse.merge({
+  Data: optional(array(ChartSampleBidAskOHLC)),
+})
+
+export interface ChartResponseCfdOnFutures extends GuardType<typeof ChartResponseCfdOnFutures> {}
+
+export const ChartResponseEtc = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseEtc extends GuardType<typeof ChartResponseEtc> {}
+
+export const ChartResponseCfdOnEtc = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnEtc extends GuardType<typeof ChartResponseCfdOnEtc> {}
+
+export const ChartResponseEtf = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseEtf extends GuardType<typeof ChartResponseEtf> {}
+
+export const ChartResponseCfdOnEtf = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnEtf extends GuardType<typeof ChartResponseCfdOnEtf> {}
+
+export const ChartResponseEtn = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseEtn extends GuardType<typeof ChartResponseEtn> {}
+
+export const ChartResponseCfdOnEtn = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnEtn extends GuardType<typeof ChartResponseCfdOnEtn> {}
+
+export const ChartResponseFund = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseFund extends GuardType<typeof ChartResponseFund> {}
+
+export const ChartResponseCfdOnFund = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnFund extends GuardType<typeof ChartResponseCfdOnFund> {}
+
+export const ChartResponseFxSpot = ChartResponse.merge({
+  Data: optional(array(ChartSampleBidAskOHLC)),
+})
+
+export interface ChartResponseFxSpot extends GuardType<typeof ChartResponseFxSpot> {}
+
+export const ChartResponseRights = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseRights extends GuardType<typeof ChartResponseRights> {}
+
+export const ChartResponseCfdOnRights = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnRights extends GuardType<typeof ChartResponseCfdOnRights> {}
+
+export const ChartResponseStock = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseStock extends GuardType<typeof ChartResponseStock> {}
+
+export const ChartResponseCfdOnStock = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseCfdOnStock extends GuardType<typeof ChartResponseCfdOnStock> {}
+
+export const ChartResponseStockIndex = ChartResponse.merge({
+  Data: optional(array(ChartSampleOHLC)),
+})
+
+export interface ChartResponseStockIndex extends GuardType<typeof ChartResponseStockIndex> {}
+
+export const ChartResponseCfdOnIndex = ChartResponse.merge({
+  Data: optional(array(ChartSampleBidAskOHLC)),
+})
+
+export interface ChartResponseCfdOnIndex extends GuardType<typeof ChartResponseCfdOnIndex> {}
