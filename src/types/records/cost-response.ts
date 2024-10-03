4ab2bebe2a016615cbc77b0e5f501b723fcd7c96
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-03 17:54:21 +0200

    Simplify the guards for cost response

--- src/types/records/cost-response.ts
+++ src/types/records/cost-response.ts
@@ -11,7 +11,6 @@ import {
   string,
   union,
 } from 'https://raw.githubusercontent.com/systematic-trader/type-guard/main/mod.ts'
-import { AssetType } from '../derives/asset-type.ts'
 import { BuySell } from '../derives/buy-sell.ts'
 import { CostAssumption } from '../derives/cost-assumption.ts'
 import { Currency3 } from '../derives/currency.ts'
@@ -19,9 +18,9 @@ import { InstrumentDisplayAndFormat } from '../derives/instrument-display-and-fo
 import { LongShortCost } from '../derives/long-short-cost.ts'
 import { PriceDisplayFormatType } from '../derives/price-display-format-type.ts'
 
-export interface CostResponse extends GuardType<typeof CostResponse> {}
+export interface CostResponseBase extends GuardType<typeof CostResponseBase> {}
 
-export const CostResponse = props({
+export const CostResponseBase = props({
   /** Currency of the selected account, used when listing currency conversion fees for the selected instrument back to the account currency. */
   AccountCurrency: Currency3,
 
@@ -30,15 +29,6 @@ export const CostResponse = props({
 
   /** Number of instrument. */
   Amount: number(),
-
-  /** Asset Type */
-  AssetType: AssetType,
-
-  /** Trade Cost for the instrument. */
-  Cost: props({
-    Long: LongShortCost,
-    Short: LongShortCost,
-  }),
 
   /** Calculation Assumptions */
   CostCalculationAssumptions: array(CostAssumption),
@@ -96,7 +86,8 @@ export const CostResponse = props({
   ),
 })
 
-export const CostResponseBond = CostResponse.merge({
+export const CostResponseBond = CostResponseBase.merge({
+  AssetType: literal('Bond'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -104,7 +95,8 @@ export const CostResponseBond = CostResponse.merge({
 
 export interface CostResponseBond extends GuardType<typeof CostResponseBond> {}
 
-export const CostResponseCfdOnCompanyWarrant = CostResponse.merge({
+export const CostResponseCfdOnCompanyWarrant = CostResponseBase.merge({
+  AssetType: literal('CfdOnCompanyWarrant'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -112,7 +104,8 @@ export const CostResponseCfdOnCompanyWarrant = CostResponse.merge({
 
 export interface CostResponseCfdOnCompanyWarrant extends GuardType<typeof CostResponseCfdOnCompanyWarrant> {}
 
-export const CostResponseCfdOnEtc = CostResponse.merge({
+export const CostResponseCfdOnEtc = CostResponseBase.merge({
+  AssetType: literal('CfdOnEtc'),
   Cost: props({
     Long: LongShortCost,
     Short: optional(LongShortCost),
@@ -121,7 +114,8 @@ export const CostResponseCfdOnEtc = CostResponse.merge({
 
 export interface CostResponseCfdOnEtc extends GuardType<typeof CostResponseCfdOnEtc> {}
 
-export const CostResponseCfdOnEtf = CostResponse.merge({
+export const CostResponseCfdOnEtf = CostResponseBase.merge({
+  AssetType: literal('CfdOnEtf'),
   Cost: props({
     Long: LongShortCost,
     Short: optional(LongShortCost),
@@ -130,7 +124,8 @@ export const CostResponseCfdOnEtf = CostResponse.merge({
 
 export interface CostResponseCfdOnEtf extends GuardType<typeof CostResponseCfdOnEtf> {}
 
-export const CostResponseCfdOnEtn = CostResponse.merge({
+export const CostResponseCfdOnEtn = CostResponseBase.merge({
+  AssetType: literal('CfdOnEtn'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -138,7 +133,8 @@ export const CostResponseCfdOnEtn = CostResponse.merge({
 
 export interface CostResponseCfdOnEtn extends GuardType<typeof CostResponseCfdOnEtn> {}
 
-export const CostResponseCfdOnFund = CostResponse.merge({
+export const CostResponseCfdOnFund = CostResponseBase.merge({
+  AssetType: literal('CfdOnFund'),
   Cost: props({
     Long: LongShortCost,
     Short: optional(LongShortCost),
@@ -147,15 +143,28 @@ export const CostResponseCfdOnFund = CostResponse.merge({
 
 export interface CostResponseCfdOnFund extends GuardType<typeof CostResponseCfdOnFund> {}
 
-export const CostResponseCfdOnFutures = CostResponse
+export const CostResponseCfdOnFutures = CostResponseBase.merge({
+  AssetType: literal('CfdOnFutures'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseCfdOnFutures extends GuardType<typeof CostResponseCfdOnFutures> {}
 
-export const CostResponseCfdOnIndex = CostResponse
+export const CostResponseCfdOnIndex = CostResponseBase.merge({
+  AssetType: literal('CfdOnIndex'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseCfdOnIndex extends GuardType<typeof CostResponseCfdOnIndex> {}
 
-export const CostResponseCfdOnRights = CostResponse.merge({
+export const CostResponseCfdOnRights = CostResponseBase.merge({
+  AssetType: literal('CfdOnRights'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -163,7 +172,8 @@ export const CostResponseCfdOnRights = CostResponse.merge({
 
 export interface CostResponseCfdOnRights extends GuardType<typeof CostResponseCfdOnRights> {}
 
-export const CostResponseCfdOnStock = CostResponse.merge({
+export const CostResponseCfdOnStock = CostResponseBase.merge({
+  AssetType: literal('CfdOnStock'),
   Cost: props({
     Long: LongShortCost,
     Short: optional(LongShortCost),
@@ -172,7 +182,8 @@ export const CostResponseCfdOnStock = CostResponse.merge({
 
 export interface CostResponseCfdOnStock extends GuardType<typeof CostResponseCfdOnStock> {}
 
-export const CostResponseCompanyWarrant = CostResponse.merge({
+export const CostResponseCompanyWarrant = CostResponseBase.merge({
+  AssetType: literal('CompanyWarrant'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -180,11 +191,18 @@ export const CostResponseCompanyWarrant = CostResponse.merge({
 
 export interface CostResponseCompanyWarrant extends GuardType<typeof CostResponseCompanyWarrant> {}
 
-export const CostResponseContractFutures = CostResponse
+export const CostResponseContractFutures = CostResponseBase.merge({
+  AssetType: literal('ContractFutures'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseContractFutures extends GuardType<typeof CostResponseContractFutures> {}
 
-export const CostResponseEtc = CostResponse.merge({
+export const CostResponseEtc = CostResponseBase.merge({
+  AssetType: literal('Etc'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -192,7 +210,8 @@ export const CostResponseEtc = CostResponse.merge({
 
 export interface CostResponseEtc extends GuardType<typeof CostResponseEtc> {}
 
-export const CostResponseEtf = CostResponse.merge({
+export const CostResponseEtf = CostResponseBase.merge({
+  AssetType: literal('Etf'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -200,7 +219,8 @@ export const CostResponseEtf = CostResponse.merge({
 
 export interface CostResponseEtf extends GuardType<typeof CostResponseEtf> {}
 
-export const CostResponseEtn = CostResponse.merge({
+export const CostResponseEtn = CostResponseBase.merge({
+  AssetType: literal('Etn'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -208,7 +228,8 @@ export const CostResponseEtn = CostResponse.merge({
 
 export interface CostResponseEtn extends GuardType<typeof CostResponseEtn> {}
 
-export const CostResponseFund = CostResponse.merge({
+export const CostResponseFund = CostResponseBase.merge({
+  AssetType: literal('Fund'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -216,15 +237,28 @@ export const CostResponseFund = CostResponse.merge({
 
 export interface CostResponseFund extends GuardType<typeof CostResponseFund> {}
 
-export const CostResponseFuturesOption = CostResponse
+export const CostResponseFuturesOption = CostResponseBase.merge({
+  AssetType: literal('FuturesOption'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseFuturesOption extends GuardType<typeof CostResponseFuturesOption> {}
 
-export const CostResponseFxForwards = CostResponse
+export const CostResponseFxForwards = CostResponseBase.merge({
+  AssetType: literal('FxForwards'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseFxForwards extends GuardType<typeof CostResponseFxForwards> {}
 
-export const CostResponseFxNoTouchOption = CostResponse.merge({
+export const CostResponseFxNoTouchOption = CostResponseBase.merge({
+  AssetType: literal('FxNoTouchOption'),
   Cost: record(
     enums(['Long', 'Short']),
     props({
@@ -237,7 +271,8 @@ export const CostResponseFxNoTouchOption = CostResponse.merge({
 
 export interface CostResponseFxNoTouchOption extends GuardType<typeof CostResponseFxNoTouchOption> {}
 
-export const CostResponseFxOneTouchOption = CostResponse.merge({
+export const CostResponseFxOneTouchOption = CostResponseBase.merge({
+  AssetType: literal('FxOneTouchOption'),
   Cost: record(
     enums(['Long', 'Short']),
     props({
@@ -250,11 +285,18 @@ export const CostResponseFxOneTouchOption = CostResponse.merge({
 
 export interface CostResponseFxOneTouchOption extends GuardType<typeof CostResponseFxOneTouchOption> {}
 
-export const CostResponseFxSpot = CostResponse
+export const CostResponseFxSpot = CostResponseBase.merge({
+  AssetType: literal('FxSpot'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseFxSpot extends GuardType<typeof CostResponseFxSpot> {}
 
-export const CostResponseFxSwap = CostResponse.merge({
+export const CostResponseFxSwap = CostResponseBase.merge({
+  AssetType: literal('FxSwap'),
   Cost: record(
     enums(['Long', 'Short']),
     LongShortCost.omit(['TotalCostPct']),
@@ -263,7 +305,8 @@ export const CostResponseFxSwap = CostResponse.merge({
 
 export interface CostResponseFxSwap extends GuardType<typeof CostResponseFxSwap> {}
 
-export const CostResponseFxVanillaOption = CostResponse.merge({
+export const CostResponseFxVanillaOption = CostResponseBase.merge({
+  AssetType: literal('FxVanillaOption'),
   Cost: record(
     enums(['Long', 'Short']),
     LongShortCost.omit(['TotalCostPct']),
@@ -272,7 +315,8 @@ export const CostResponseFxVanillaOption = CostResponse.merge({
 
 export interface CostResponseFxVanillaOption extends GuardType<typeof CostResponseFxVanillaOption> {}
 
-export const CostResponseRights = CostResponse.merge({
+export const CostResponseRights = CostResponseBase.merge({
+  AssetType: literal('Rights'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -280,7 +324,8 @@ export const CostResponseRights = CostResponse.merge({
 
 export interface CostResponseRights extends GuardType<typeof CostResponseRights> {}
 
-export const CostResponseStock = CostResponse.merge({
+export const CostResponseStock = CostResponseBase.merge({
+  AssetType: literal('Stock'),
   Cost: props({
     Long: LongShortCost,
   }),
@@ -288,10 +333,82 @@ export const CostResponseStock = CostResponse.merge({
 
 export interface CostResponseStock extends GuardType<typeof CostResponseStock> {}
 
-export const CostResponseStockIndexOption = CostResponse
+export const CostResponseStockIndexOption = CostResponseBase.merge({
+  AssetType: literal('StockIndexOption'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseStockIndexOption extends GuardType<typeof CostResponseStockIndexOption> {}
 
-export const CostResponseStockOption = CostResponse
+export const CostResponseStockOption = CostResponseBase.merge({
+  AssetType: literal('StockOption'),
+  Cost: props({
+    Long: LongShortCost,
+    Short: LongShortCost,
+  }),
+})
 
 export interface CostResponseStockOption extends GuardType<typeof CostResponseStockOption> {}
+
+export const CostResponse = {
+  Bond: CostResponseBond,
+  CfdOnCompanyWarrant: CostResponseCfdOnCompanyWarrant,
+  CfdOnEtc: CostResponseCfdOnEtc,
+  CfdOnEtf: CostResponseCfdOnEtf,
+  CfdOnEtn: CostResponseCfdOnEtn,
+  CfdOnFund: CostResponseCfdOnFund,
+  CfdOnFutures: CostResponseCfdOnFutures,
+  CfdOnIndex: CostResponseCfdOnIndex,
+  CfdOnRights: CostResponseCfdOnRights,
+  CfdOnStock: CostResponseCfdOnStock,
+  CompanyWarrant: CostResponseCompanyWarrant,
+  ContractFutures: CostResponseContractFutures,
+  Etc: CostResponseEtc,
+  Etf: CostResponseEtf,
+  Etn: CostResponseEtn,
+  Fund: CostResponseFund,
+  FuturesOption: CostResponseFuturesOption,
+  FxForwards: CostResponseFxForwards,
+  FxNoTouchOption: CostResponseFxNoTouchOption,
+  FxOneTouchOption: CostResponseFxOneTouchOption,
+  FxSpot: CostResponseFxSpot,
+  FxSwap: CostResponseFxSwap,
+  FxVanillaOption: CostResponseFxVanillaOption,
+  Rights: CostResponseRights,
+  Stock: CostResponseStock,
+  StockIndexOption: CostResponseStockIndexOption,
+  StockOption: CostResponseStockOption,
+} as const
+
+export type CostResponse = {
+  Bond: CostResponseBond
+  CfdOnCompanyWarrant: CostResponseCfdOnCompanyWarrant
+  CfdOnEtc: CostResponseCfdOnEtc
+  CfdOnEtf: CostResponseCfdOnEtf
+  CfdOnEtn: CostResponseCfdOnEtn
+  CfdOnFund: CostResponseCfdOnFund
+  CfdOnFutures: CostResponseCfdOnFutures
+  CfdOnIndex: CostResponseCfdOnIndex
+  CfdOnRights: CostResponseCfdOnRights
+  CfdOnStock: CostResponseCfdOnStock
+  CompanyWarrant: CostResponseCompanyWarrant
+  ContractFutures: CostResponseContractFutures
+  Etc: CostResponseEtc
+  Etf: CostResponseEtf
+  Etn: CostResponseEtn
+  Fund: CostResponseFund
+  FuturesOption: CostResponseFuturesOption
+  FxForwards: CostResponseFxForwards
+  FxNoTouchOption: CostResponseFxNoTouchOption
+  FxOneTouchOption: CostResponseFxOneTouchOption
+  FxSpot: CostResponseFxSpot
+  FxSwap: CostResponseFxSwap
+  FxVanillaOption: CostResponseFxVanillaOption
+  Rights: CostResponseRights
+  Stock: CostResponseStock
+  StockIndexOption: CostResponseStockIndexOption
+  StockOption: CostResponseStockOption
+}
