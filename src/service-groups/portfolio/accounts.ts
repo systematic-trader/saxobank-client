92649fa324e53baaa4e6c96211d424edb9ad014e
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-14 12:54:15 +0200

    Remove reset account resource

--- src/service-groups/portfolio/accounts.ts
+++ src/service-groups/portfolio/accounts.ts
@@ -1,15 +1,12 @@
 import type { ServiceGroupClient } from '../../service-group-client.ts'
-import { Account } from './accounts/account.ts'
 import { Me } from './accounts/me.ts'
 
 export class Accounts {
   readonly me: Me
-  readonly account: Account
 
   constructor({ client }: { readonly client: ServiceGroupClient }) {
     const serviceGroupClient = client.appendPath('v1/accounts')
 
     this.me = new Me({ client: serviceGroupClient })
-    this.account = new Account({ client: serviceGroupClient })
   }
 }
