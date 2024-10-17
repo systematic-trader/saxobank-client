8b0cb22a9d143c169f27ebfe4d24eecaca77954f
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-17 08:42:08 +0200

    Initial implementation for FxForwards orders

--- src/service-groups/portfolio/accounts.ts
+++ src/service-groups/portfolio/accounts.ts
@@ -1,12 +1,15 @@
 import type { ServiceGroupClient } from '../../service-group-client.ts'
+import { Account } from './accounts/account.ts'
 import { Me } from './accounts/me.ts'
 
 export class Accounts {
+  readonly account: Account
   readonly me: Me
 
   constructor({ client }: { readonly client: ServiceGroupClient }) {
     const serviceGroupClient = client.appendPath('v1/accounts')
 
+    this.account = new Account({ client: serviceGroupClient })
     this.me = new Me({ client: serviceGroupClient })
   }
 }
